799e6066515b441dbb4980989222e88ee839a598
Lukasz Kowalczyk <lukasz.kowalczyk.dev@gmail.com>
2023-09-18 10:56:48 +0200

    CRUD

---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,33 @@
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _contex;
+
+            public Handler(DataContext contex)
+            {
+                _contex = contex;
+            }
+
+            public async Task Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _contex.Activities.FindAsync(request.Id);
+
+                _contex.Remove(activity);
+
+                await _contex.SaveChangesAsync();
+            }
+        }
+    }
+}
